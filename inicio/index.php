2ce7511da2041885b26e6537bd67f8681e200a10
Leonard <99060703+LeonardAguirree@users.noreply.github.com>
2022-02-13 13:54:07 -0600

    Add files via upload

---
+++ inicio/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"> 
+	<title>Leonnard Studio</title>
+</head>
+<body>
+	<header>
+		<div id="icon">
+			<img src="../icons/logo.png">
+			<span id="menu_icon"><img src="../icons/menu_icon.png"></span>
+		</div>	
+		<nav id="navigation">
+			<ul>
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Ilustraciones</a></li>
+				<li><a href="">Manga</a></li>
+				<li><a href="">Videojuego</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div id="post">
+		<section>
+			<article>
+				<div></div>
+				<section></section>
+				<section></section>
+			</article>
+		</section>
+	</div>
+	<aside id="info"></aside>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="scripts.js"></script>
+</body>
+</html>
